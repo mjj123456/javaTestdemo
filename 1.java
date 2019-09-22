abca915d0a949edabaf2998086746f00fef516d4
sunny <691124379@qq.com>
2019-09-22 19:58:56 +0800

    再次修改代码

--- 1.java
+++ 1.java
@@ -3,6 +3,7 @@ public class controller {
     @Reference
     private HelloService helloService;
 
+	//修改了这行代码
     @RequestMapping("/hello")
     public String hello(String name) {
         return helloService.speakHello(name);
